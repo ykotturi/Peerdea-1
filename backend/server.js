066bb945ed6b98d21b1f8c33da06bb2423610eae
Michael Chang <machang@andrew.cmu.edu>
2019-04-17 19:56:18 -0400

    added group api endpoints

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ var cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const Data = require("./data");
+const Group = require("./src/group")
+const Idea = require("./src/idea")
+const Member = require("./src/member")
+const Review = require("./src/review")
 
 const API_PORT = 3001;
 const app = express();
@@ -79,6 +83,57 @@ router.post("/putData", (req, res) => {
   data.message = message;
   data.id = id;
   data.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+
+
+// this is our get method
+// this method fetches all available data in our database
+router.get("/getGroup", (req, res) => {
+   Group.find((err, data) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: data });
+  });
+});
+
+// this is our update method
+// this method overwrites existing data in our database
+router.post("/updateGroup", (req, res) => {
+  const { id, update } = req.body;
+  console.log(req.body);
+  Group.findOneAndUpdate({ "_id" : id}, update, err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+// this is our delete method
+// this method removes existing data in our database
+router.delete("/deleteGroup", (req, res) => {
+  const { id } = req.body;
+  console.log(req.body);
+  Group.findOneAndDelete({ "_id" : id}, err => {
+    if (err) return res.send(err);
+    return res.json({ success: true });
+  });
+});
+
+// this is our create methid
+// this method adds new data in our database
+router.post("/putGroup", (req, res) => {
+  let group = new Group(req.body);
+
+  console.log(req.body);
+//  if ((!id && id !== 0) || !message) {
+//    return res.json({
+//      success: false,
+//      error: "INVALID INPUTS"
+//    });
+//  }
+  group.save(err => {
     if (err) return res.json({ success: false, error: err });
     return res.json({ success: true });
   });
