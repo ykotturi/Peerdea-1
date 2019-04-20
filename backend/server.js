24bc58505325228fcad4e840bb84c0724ba05bbb
machang19 <31482732+machang19@users.noreply.github.com>
2019-04-20 11:13:58 -0400

    remove dead code from server.js

--- backend/server.js
+++ backend/server.js
@@ -81,12 +81,7 @@ router.post("/putGroup", (req, res) => {
   let group = new Group(req.body);
 
   console.log(req.body);
-//  if ((!id && id !== 0) || !message) {
-//    return res.json({
-//      success: false,
-//      error: "INVALID INPUTS"
-//    });
-//  }
+
   group.save(err => {
     if (err) return res.json({ success: false, error: err });
     return res.json({ success: true });
@@ -141,12 +136,7 @@ router.post("/putConcept", (req, res) => {
   let concept = new Concept(req.body);
 
   console.log(req.body);
-//  if ((!id && id !== 0) || !message) {
-//    return res.json({
-//      success: false,
-//      error: "INVALID INPUTS"
-//    });
-//  }
+
   Concept.save(err => {
     if (err) return res.json({ success: false, error: err });
     return res.json({ success: true });
