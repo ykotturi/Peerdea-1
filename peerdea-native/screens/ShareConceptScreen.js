51c6e43343c69e70b406fbdaaa573373c158b737
Yasmine Kotturi <ykotturi@andrew.cmu.edu>
2019-04-22 23:43:31 -0400

    basic concept sharing features in place

--- peerdea-native/screens/ShareConceptScreen.js
+++ peerdea-native/screens/ShareConceptScreen.js
@@ -1,7 +1,53 @@
 import React, { Component } from 'react';
-import { Button, Image, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Button, Image, View, StyleSheet, Text, TouchableOpacity, TextInput, Alert } from 'react-native';
 // because we're using mangaged apps version of expo (and not bare version):
 import { ImagePicker, Permissions, Camera } from 'expo';
+
+
+
+export class ConceptAuthor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Enter your name here:' };
+  }
+
+
+  // first, should check to make sure valid group name (numbers and letters only, longer than 5 characters)
+  // then, there should be some check as to whether the group name is already taken
+  // if not already taken, submit API post request to create a new group
+  // if it is taken, catch error and say group name already taken
+  render() {
+    return (
+      <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}
+      />
+    );
+  }
+}
+
+export class ConceptDescription extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Describe your concept here' };
+  }
+
+
+  // first, should check to make sure valid group name (numbers and letters only, longer than 5 characters)
+  // then, there should be some check as to whether the group name is already taken
+  // if not already taken, submit API post request to create a new group
+  // if it is taken, catch error and say group name already taken
+  render() {
+    return (
+      <TextInput
+        style={{height: 70, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}
+      />
+    );
+  }
+}
 
 
 export default class ConceptImagePicker extends React.Component {
@@ -23,7 +69,14 @@ export default class ConceptImagePicker extends React.Component {
     let { image } = this.state;
 
     return (
+
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+      <Text style={styles.getStartedText}>Share a concept with your group</Text>
+
+      <ConceptAuthor/>
+
+      
         <Button
           title="Pick an image from camera roll"
           onPress={this._pickImage}
@@ -33,7 +86,18 @@ export default class ConceptImagePicker extends React.Component {
           onPress={this._takePicture}
         />
         {image &&
-          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+
+        <ConceptDescription/>
+
+        <Button
+          onPress={() => {
+            Alert.alert('Thanks for sharing!');
+          }}
+          title="Share concept with my group"
+          color="#841584"
+          accessibilityLabel="Share concept with my group"
+        />
       </View>
     );
   }
@@ -70,3 +134,17 @@ export default class ConceptImagePicker extends React.Component {
   };
 
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+});
