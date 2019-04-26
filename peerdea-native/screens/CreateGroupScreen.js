b579a359c0f1b195c447715673f2e6f393f4b3db
judyzhang25 <31805219+judyzhang25@users.noreply.github.com>
2019-04-26 16:09:52 -0400

    small changes

--- peerdea-native/screens/CreateGroupScreen.js
+++ peerdea-native/screens/CreateGroupScreen.js
@@ -27,7 +27,7 @@ export default class CreateGroupScreen extends React.Component {
   async onCreate() {
     try {
       //check if the group exists first
-      const checkRes = await fetch('http://128.237.219.139:3001/api/getGroupByName?name=' + this.state.groupName, {method: 'GET'});
+      const checkRes = await fetch('http://128.237.194.92:3001/api/getGroupByName?name=' + this.state.groupName, {method: 'GET'});
       const checkResJson = await checkRes.json();
       console.log("print " + JSON.stringify(checkResJson.data));
 
--- peerdea-native/screens/JoinGroupScreen.js
+++ peerdea-native/screens/JoinGroupScreen.js
@@ -23,7 +23,7 @@ export default class JoinGroupScreen extends React.Component {
     console.log("join pressed "+ this.state.groupName);
     try {
       //check if the group exists first
-      const checkRes = await fetch('http://128.237.219.139:3001/api/getGroupByName?name=' + this.state.groupName, {method: 'GET'});
+      const checkRes = await fetch('http://128.237.194.92:3001/api/getGroupByName?name=' + this.state.groupName, {method: 'GET'});
       const checkResJson = await checkRes.json();
       console.log("print " + JSON.stringify(checkResJson.data));
 
