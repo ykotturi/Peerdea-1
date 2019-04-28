b404069ae2b721ee74ba0db8e9499b0d8268d4dd
Michael Chang <machang@andrew.cmu.edu>
2019-04-28 17:23:35 -0400

    initial display of concepts

--- peerdea-native/screens/GiveFeedbackScreen.js
+++ peerdea-native/screens/GiveFeedbackScreen.js
@@ -10,197 +10,53 @@ import { Buffer } from 'buffer';
 // PICK UP HERE
 //TODO: change infrastructure of this file to make state hold multple values of what a concet is 
 
-export default class ShareConcept extends React.Component {
+export default class GiveFeedback extends React.Component {
   state = {
-    author: 'Enter your name here',
-    author2: null,
-    image: null,
-    image1: null,
-    image2: null,
-    story: 'This is my concepts story!',
-    imageBase64: null,
+    author: null,
+    group_id: groupID
+    concepts: []
   };
 
 
-  askPermissionsAsync = async () => {
-      await Permissions.askAsync(Permissions.CAMERA_ROLL);
-      await Permissions.askAsync(Permissions.CAMERA);
-      // probably need to do something to verify that permissions
-      // were actually granted
-   };
+  componentDidMount() {
+    const {navigation} = this.props;
+    const screenName = navigation.getParam('name', 'NO NAME');
+    const groupID = navigation.getParam('groupID', '5cb7d06d5de2e75344837340');
+    this.setState({author: screenName, group_id: groupID});
+    const checkRes = await fetch('http://104.40.20.156/api/getConceptsByGroup?groupID=' + groupID, {method: 'GET'});
+    const checkResJson = await checkRes.json();
+    this.setState({concepts: checkResJson.data})
+
+  }
 
 
   render() {
-    let image = this.state.image;
-    let author2 = this.state.author2;
-    let image1 = this.state.image1;
-    let image2 = this.state.image2; 
+    var conceptViews = [];
 
+	for each (var concept in this.state.concepts){
+        let buff = new Buffer(concept.media.data);
+        const base64data = buff.toString('base64');
+        const uriString = `data:image/gif;base64,${base64data}`;
+		conceptViews.push(
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				{uriString &&
+                <Image source={{ uri: uriString }} style={{ width: 200, height: 200 }} />}
+                <Text> {concept.name} </Text>
+                <Text> {concept.description} </Text>
+			</View>
+		)
+	}
     return (
       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={styles.getStartedText}>Share a concept with your group</Text>
-      <TextInput
-        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-        onChangeText={(text) => this.setState({author:text})}
-        value={this.state.author}
-      />
-      <Button
-        title="Pick an image from camera roll"
-        onPress={this._pickImage}
-      />
-      <Button
-        title="Take a picture"
-        onPress={this._takePicture}
-      />
-      {image &&
-      <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-      {image1 &&
-      <Image source={{ uri: image1 }} style={{ width: 200, height: 200 }} />}
-      {image2 &&
-      <Image source={{ uri: image2 }} style={{ width: 200, height: 200 }} />}
-      
-      <Text> {this.state.author2} </Text>
-      <TextInput
-        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-        onChangeText={(text) => this.setState({story:text})}
-        value={this.state.story}
-      />
-      <Button
-        onPress={() => { this._sendConcept();}}
-        title="Share concept with my group"
-        color="#841584"
-        accessibilityLabel="Share concept with my group"
-      />
-      <Button
-        onPress={() => { this._getConcepts();}}
-        title="Get concepts"
-        color="#841584"
-        accessibilityLabel="Get concepts"
-      />
+      <Text style={styles.getStartedText}>Review Group Concepts</Text>
+        { conceptViews }
       </View>
       </ScrollView>
     );
   }
 
-  _sendConcept = () => {
 
-      
-      // get requests to get users group keyword
-
-      var buff = new Buffer(this.state.imageBase64, 'base64');
-      
-      let data = {
-        method: 'POST',
-        credentials: 'same-origin',
-        mode: 'same-origin',
-        body: JSON.stringify({
-          group_id: "5cb7d06d5de2e75344837340",
-              name: this.state.author,
-              media: { 
-            data: buff, 
-            contentType: 'image/png'},
-              description: this.state.story,
-        }),
-        headers: {
-          'Accept':       'application/json',
-          'Content-Type': 'application/json',
-          // 'X-CSRFToken':  cookie.load('csrftoken')
-          }
-       }
-        Alert.alert('Thanks for sharing!');
-        return fetch('http://104.40.20.156/api/putConcept', data)
-        .then(function(response){
-          return response.json();
-        })
-        .then(function(json){
-         console.log('suuccess');
-        })
-        .catch(function(error) {
-        console.log('There has been a problem with your fetch operation: ' + error.message);
-         // ADD THIS THROW error
-          throw error;
-        });
-
-   }
-
-
-  _getConcepts = () => {
-
-   
-
-    return fetch('http://104.40.20.156/api/getConcepts', {method: 'GET'})
-    .then((response) => response.json())
-        .then((responseJson) => {
-          let buff = new Buffer(responseJson.data[0].media.data);
-           // console.log(responseJson.data[0].media.data);
-           // image stored in database as buffer, so this line converts buffer to type base64
-           // console.log('RESPONSEJSON.DATA[0].MEDIA.DATA.tostring is ' + responseJson.data[0].media.data.toString('base64'));
-           const base64data = buff.toString('base64');
-           // takes base64 and turns into uri
-           const uriString = `data:image/gif;base64,${base64data}`;
-           // console.log('uriString is ' + uriString);
-           let buff2 = new Buffer(responseJson.data[4].media.data);
-           // console.log(responseJson.data[0].media.data);
-           // image stored in database as buffer, so this line converts buffer to type base64
-           // console.log('RESPONSEJSON.DATA[0].MEDIA.DATA.tostring is ' + responseJson.data[0].media.data.toString('base64'));
-           const base64data2 = buff2.toString('base64');
-           // takes base64 and turns into uri
-           const uriString2 = `data:image/gif;base64,${base64data2}`;
-           console.log('AUTHORIS ' + responseJson.data[4].name);
-           let author = responseJson.data[4].name;
-           this.setState({
-              image1: uriString,
-              image2: uriString2,
-              author2: author,
-           })
-        })
-        .catch((error) => {
-           console.error(error);
-        });
-  }
-
-
-  //underscore before function name to distinguish internal methods from the lifecycle methods of react
-  _pickImage = async () => {
-    await this.askPermissionsAsync();
-    let result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-      base64: true,
-    });
-
-    // probably need some express api post call to add "result" variable to database
-    
-    //uncomment below if you'd like to see what RESULT is, printed in console in expo
-    //console.log('RESULT IS' + result);
-    //console.log('RESULT.URI IS' + result.uri);
-
-    // console.log('RESULT IS' + result);
-    // console.log('RESULT.URI IS' + result.uri);
-    // console.log('THIS STATE is' + this.state);
-
-    if (!result.cancelled) {
-      this.setState({ image: result.uri, imageBase64: result.base64 });
-    }
-  };
-
-  _takePicture = async () => {
-    await this.askPermissionsAsync();
-    let result = await ImagePicker.launchCameraAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-      base64: true,
-    });
-
-    // console.log('RESULT IS' + result);
-    // console.log('RESULT.URI IS' + result.uri);
-   
-    
-    if (!result.cancelled) {
-      this.setState({  image: result.uri,imageBase64 : result.base64 });
-    }
-  };
 
 }
 
--- peerdea-native/screens/HomeScreen.js
+++ peerdea-native/screens/HomeScreen.js
@@ -47,9 +47,12 @@ export default class HomeScreen extends React.Component {
             color="#841584"
             accessibilityLabel="Join an existing group"
           />
-
+        this.props.navigation.navigate('ShareConcept');
           <Button
-            onPress={() => navigate('GiveFeedback')}
+            onPress={() => navigate('GiveFeedback', {
+                name: 'Michael',
+                groupID: '5cb7d06d5de2e75344837340'
+                })}
             title="Give Feedback"
             color="#841584"
             accessibilityLabel="Give Feedback"
